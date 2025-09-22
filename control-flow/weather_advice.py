eae7dce636becaebd7257b10644f06c0f7425f0e
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-22 10:04:30 +0300

    Spelling mistakes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,10 +2,10 @@ current_weather=input("What's the weather like today?(sunny/rainy/cold):").lower
 if current_weather in ["sunny","rainy","cold"]:
     match current_weather:
         case "sunny":
-            print("Wera a t-shirt and sunglasses.")
+            print("Wear a t-shirt and sunglasses.")
         case "rainy":
             print("Don't forget your umbrella and raincoat.")
         case "cold":
-            print("Make sure to wear a warm coat and scraf.")
+            print("Make sure to wear a warm coat and scarf.")
 else:
     print("Sorry,I don't have recommendation for this weather.")
