686dabf40c863bc5c7dd2a340aa7601d5e9ee627
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-22 10:07:52 +0300

    Used if else instead of match case

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
-current_weather=input("What's the weather like today?(sunny/rainy/cold):").lower()
-if current_weather in ["sunny","rainy","cold"]:
-    match current_weather:
-        case "sunny":
-            print("Wear a t-shirt and sunglasses.")
-        case "rainy":
-            print("Don't forget your umbrella and raincoat.")
-        case "cold":
-            print("Make sure to wear a warm coat and scarf.")
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry,I don't have recommendation for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
