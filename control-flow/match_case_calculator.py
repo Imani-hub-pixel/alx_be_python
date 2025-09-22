e3cb19897ac02d5d39677218289d14386d53d1ac
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-22 10:27:24 +0300

    match_case_calculator.py file added

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+First_number=int(input("Enter the first number:"))
+second_number=int(input("Enter the second number:"))
+operation=input("Choose the operation(+,-,*,/):")
+match operation:
+    case "+":
+        result=First_number+second_number
+    case "-":
+        result=First_number-second_number
+    case "*":
+        result=First_number*second_number  
+    case "/":
+        if second_number!=0:
+            result=First_number/second_number
+        else:
+            print("Cannot divide by zero")
+print("The result is ",result)
