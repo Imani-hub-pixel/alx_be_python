32a269bc2c49db46999aca8714e84b9689ab3cba
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-22 09:59:05 +0300

    Added control flow directory with weather_advice.py file

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+current_weather=input("What's the weather like today?(sunny/rainy/cold):").lower()
+if current_weather in ["sunny","rainy","cold"]:
+    match current_weather:
+        case "sunny":
+            print("Wera a t-shirt and sunglasses.")
+        case "rainy":
+            print("Don't forget your umbrella and raincoat.")
+        case "cold":
+            print("Make sure to wear a warm coat and scraf.")
+else:
+    print("Sorry,I don't have recommendation for this weather.")
