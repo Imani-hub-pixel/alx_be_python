750169e7e07de3e29e2277d5b57f09327c7b9816
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-22 10:30:30 +0300

    match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 First_number=int(input("Enter the first number:"))
 second_number=int(input("Enter the second number:"))
-operation=input("Choose the operation(+,-,*,/):")
+operation=input("Choose the operation(+,-,*,/:").strip()
 match operation:
     case "+":
         result=First_number+second_number
