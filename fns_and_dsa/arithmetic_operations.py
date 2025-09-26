64bfbb96f110472c274be8362f65767e2c96ce63
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-27 01:34:40 +0300

    functions and data structures

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation=['add','subtarct','multiply','divide']):
+def perform_operation(num1,num2,operation):
     if operation=='add':
         return num1+num2
     elif operation=='subtract':
