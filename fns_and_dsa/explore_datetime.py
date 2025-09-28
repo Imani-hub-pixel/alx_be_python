b1028f513efa278fbc7bfa1f554f0d007909e8de
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-28 15:16:16 +0300

    ..

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     current_date=datetime.now()
     return f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}"
-current_days = int(input("Enter number of days to add to the current date: "))
+current_days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date(current_days):
     future_date=datetime.now()+timedelta(days=current_days)
