846ee4ac2febacdde2bd5626fdd1894f5c83aeb0
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-28 15:06:05 +0300

    spelling mistakes

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,14 +1,15 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
-def convert_to_celsius(farenheit):
-    return (farenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-def convert_to_farenheit(celsius):
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 temperature = input("Enter the temperature you want to convert :")
-unit=input("Is this temperature in (C)elsius or (F)arenheit?(C/F) ").strip().upper()
+unit=input("Is this temperature in Celsius or Fahrenheit?(C/F) ").strip().upper()
 if unit=='C':
-    converted_temp=convert_to_farenheit(float(temperature))
+    converted_temp=convert_to_fahrenheit(float(temperature))
     print(f"{temperature}°C is {converted_temp:.2f}°F")
 elif unit=='F':
     converted_temp=convert_to_celsius(float(temperature))
