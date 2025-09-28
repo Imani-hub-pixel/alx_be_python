d5f7ba9226563637d1dc53c8e1575e5721a146b1
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-28 14:40:21 +0300

    Explored datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date=datetime.now()
+    print(f" current date and time: {current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}:{current_date.second}")
+current_days=int(input("Enter number of days to add to the current date: "))
+def calculate_future_date(curret_days):
+    future_date=datetime.now()+timedelta(days=curret_days)
+    print(f"Future date: {future_date.year}-{future_date.month}-{future_date.day} ")
+def main():
+    display_current_datetime()
+    calculate_future_date(current_days) 
+if __name__=="__main__":
+    main()
