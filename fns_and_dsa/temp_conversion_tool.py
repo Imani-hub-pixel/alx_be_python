362b6c1e7a9939b8d1829a6d2248f0e93b607f33
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-28 15:11:01 +0300

    ValueError for a non numeric input

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,12 +5,14 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-
-temperature = input("Enter the temperature to convert: ")
-unit=input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
-if unit=='C':
-    converted_temp=convert_to_fahrenheit(float(temperature))
-    print(f"{temperature}°C is {converted_temp:.2f}°F")
-elif unit=='F':
-    converted_temp=convert_to_celsius(float(temperature))
-    print(f"{temperature}°F is {converted_temp:.2f}°C")
+try:
+    temperature = float(("Enter the temperature to convert: "))
+    unit=input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    if unit=='C':
+        converted_temp=convert_to_fahrenheit(float(temperature))
+        print(f"{temperature}°C is {converted_temp:.2f}°F")
+    elif unit=='F':
+        converted_temp=convert_to_celsius(float(temperature))
+        print(f"{temperature}°F is {converted_temp:.2f}°C")
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value")
