81b073a6df7103b9f3618dce5940f200e52b38f3
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-09-28 15:19:12 +0300

    ...

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,7 +18,7 @@ def main():
         choice = int(choice_input)
 
         if choice == 1:
-            item=input("Enter the item to add:")
+            item=input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added to the list")
             
