e17a55f21a8595048787e731179d71fc9634d761
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-03 22:07:00 +0300

    Nested exception handling

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,12 +2,16 @@ def safe_divide(numerator,denominator):
     try:
         numerator=float(numerator)
         denominator=float(denominator)
+    
+        try:
+            result=numerator/denominator
+            return f"The result of the division is {result}"
+
+        
+        except ZeroDivisionError:
+            print("Error: Cannot divide by zero.")
+
+    
     except ValueError:
         print("Error: Please enter numeric values only.")
-    try:
-        result=numerator/denominator
-        
-    except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
-
-    return f"The result of the division is {result}"
+    
