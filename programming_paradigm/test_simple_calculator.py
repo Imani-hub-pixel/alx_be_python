9c22d890ee7c11b1d3233e88d3077320db48faf9
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-03 23:10:48 +0300

    Testing

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -21,12 +21,12 @@ class TestCalculator:
         result=self.calc.subtract(2,4)
         self.assertEqual(result,-2)
     def test_multiply(self):
-        self.assertEqual(self.calc.subtract(10,5 ), 50)
+        self.assertEqual(self.calc.multiply(10,5 ), 50)
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(2,0)
     def test_divide(self):
-         self.assertEqual(self.calc.subtract(10,5 ), 2)
+         self.assertEqual(self.calc.divide(10,5 ), 2)
 if __name__=="__main__":
     unittest.main()
     
