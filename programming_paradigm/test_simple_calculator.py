e691399c20bb48455e46e2b1a41e86a8d63849b7
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-03 23:09:25 +0300

    Testing

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,8 +5,7 @@ class TestCalculator:
     def setUp(self):
         self.calc = SimpleCalculator()
     def test_addition(self):
-        result=self.calc.add(4,3)
-        self.assertEqual(result,7)
+        self.assertEqual(self.calc.add(2, 3), 5)
     def test_add_negative(self):
         result=self.calc.add(-2,5)
         self.assertEqual(result,3)
@@ -17,20 +16,17 @@ class TestCalculator:
         result=self.calc.subtract(-7,-4)
         self.assertEqual(result,-11)
     def test_subtraction(self):
-        result=self.calc.subtract(4,2)
-        self.assertEqual(result,2)
+       self.assertEqual(self.calc.subtract(10,5 ), 5)
     def test_subtract_negative(self):
         result=self.calc.subtract(2,4)
         self.assertEqual(result,-2)
     def test_multiply(self):
-        result=self.calc.multiply(4,2)
-        self.assertEqual(result,8)
+        self.assertEqual(self.calc.subtract(10,5 ), 50)
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(2,0)
     def test_divide(self):
-        result=self.calc.divide(4,2)
-        self.assertEqual(result,2)
+         self.assertEqual(self.calc.subtract(10,5 ), 2)
 if __name__=="__main__":
     unittest.main()
     
