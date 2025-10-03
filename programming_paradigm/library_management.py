cae9538ccbb7ea504a38df23f3d733ea4e2971aa
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-03 23:52:44 +0300

    OOP

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,22 +2,23 @@ class Book:
     def __init__(self,title,author,_is_checked_out):
         self.title=title
         self.author=author
-        self.___is_checked_out=_is_checked_out
+        self.__is_checked_out=_is_checked_out
     def check_out_book(self):
-        return self.___is_checked_out
+        self.__is_checked_out
     def return_book(self):
-        self.___is_checked_out=False
-        return self.___is_checked_out
-
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True   # checkout successful
+        return False  
         
 class Library(Book):
     def __init__(self):
-        self.___books=[]
+        self.__books=[]
     def add_book(self,title):
-        self.___books.append(title)
+        self.__books.append(title)
     def check_out_book(self,title):
-        self.___books.append(title)
+        self.__books.append(title)
     def return_book(self,title):
-        self.___books.append(title)
+        self.__books.append(title)
     def list_available_books(self):
-        return self.___books
+        return self.__books
