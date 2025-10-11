89991752623508e81b9f5d4cee9f4318571e3c91
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-11 17:41:48 +0300

    Polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("Derived classes need to override this method")
+    
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+    def area(Self):
+        return Self.length*Self.width
+    def __str__(self):
+        return f"The area of the Rectangle is: {self.area} "
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return math.pi*self.radius*self.radius
+    def __str__(self):
+        return f"The area of the Circle is: {self.area} "
