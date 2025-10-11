8addc6a015da75a869a3e90c303157cd8f05f244
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-11 17:43:51 +0300

    Polymorphism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -15,6 +15,6 @@ class Circle(Shape):
     def __init__(self,radius):
         self.radius=radius
     def area(self):
-        return math.pi*self.radius*self.radius
+        return math.pi*self.radius**2
     def __str__(self):
         return f"The area of the Circle is: {self.area} "
