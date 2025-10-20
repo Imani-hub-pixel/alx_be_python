d033c729007eaad0262236d024f33e4933176178
Faith Chepngeno <faithchepngeno850@gmil.com>
2025-10-21 00:52:38 +0300

    DB

--- intro_to_db/db_proj.py
+++ intro_to_db/db_proj.py
@@ -7,7 +7,7 @@ mydb=mysql.connector.connect(
     database="LibraryManagementSystem"
 )
 mycursor=mydb.cursor()
-"""
+
 title=input("Enter book title: ")
 author=input("Enter book author: ")
 isbn=input("Enter book ISBN: ")
@@ -17,15 +17,15 @@ val=(title, author, isbn)
 mycursor.execute(sql, val)
 mydb.commit()
 
-"""
+
 #allow users to search for books by title
-"""title_search=input("Enter book title to search: ")
+title_search=input("Enter book title to search: ")
 sql="SELECT * FROM books WHERE title=%s"
 val=(title_search,)
 mycursor.execute(sql, val)
 results=mycursor.fetchall()
 for row in results:
-    print(row)"""
+    print(row)
 
 #Printing the details of all books in the database
 mycursor.execute("SELECT * FROM books ORDER BY id ASC")
@@ -34,9 +34,9 @@ for row in rows:
     print(row)
 
 
-"""sql="DELETE FROM books WHERE id=1"
+sql="DELETE FROM books WHERE id=1"
 mycursor.execute(sql)
-mydb.commit()"""
+mydb.commit()
 
 mycursor.close()
 mydb.close()
